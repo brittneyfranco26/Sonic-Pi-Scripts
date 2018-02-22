5f48732428031035b9daf5a063512345609671c0
brittneyfranco26 <brittneyfranco26@users.noreply.github.com>
2018-02-22 13:48:16 -0800

    Update strangerthings.rb

--- strangerthings.rb
+++ strangerthings.rb
@@ -1,18 +1,22 @@
 #Stranger Things Main Theme
 # NOTE: Each measure's total sleep adds up to 4 because the time signature is 4/4
 use_bpm 160
-use_synth :saw
+use_synth :dpulse
 stranger_things = [:c2,:e2,:g2,:b2,:c3,:b2,:g2,:e2]
 index = 0
+
 live_loop :theme_intro do
   with_fx :distortion do
     8.times do
-      #Will play the first variable which is c2
+      #Will play the first variable in the array which is c2
       play stranger_things[index]
       sleep 0.5
       index= index + 1
-      #will then play the following variables which are in the array above
-     end
-   index =0
+      #will then play the following variables which are in the array aboven
+      
+      
+    end
+    index=0
   end
 end
+
