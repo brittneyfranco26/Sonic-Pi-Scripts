e0c13d4009e22647fceef80366f895a5789af90f
brittneyfranco26 <brittneyfranco26@users.noreply.github.com>
2018-02-22 13:47:30 -0800

    Update strangerthings.rb

--- strangerthings.rb
+++ strangerthings.rb
@@ -4,13 +4,15 @@ use_bpm 160
 use_synth :saw
 stranger_things = [:c2,:e2,:g2,:b2,:c3,:b2,:g2,:e2]
 index = 0
-
-8.times do
+live_loop :theme_intro do
   with_fx :distortion do
-    #Will play the first variable which is c2
-    play stranger_things[index]
-    sleep 0.5
-    index= index + 1
-    #will then play the following variables which are in the array above
+    8.times do
+      #Will play the first variable which is c2
+      play stranger_things[index]
+      sleep 0.5
+      index= index + 1
+      #will then play the following variables which are in the array above
+     end
+   index =0
   end
 end
